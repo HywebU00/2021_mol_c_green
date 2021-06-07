1d6eaf521bb8844ff6e9e370858948dee06792ad
anniewu <annie@hyweb.com.tw>
2021-06-07 15:44:39 +0800

    20210607勞動部增加頁面

--- js/hyui.js
+++ js/hyui.js
@@ -541,9 +541,11 @@ $(function() {
             ww = _window.outerWidth();
             tabSet();
             tabSet2();
+            tabSet3();
         }, 50);
     });
 
+    // 最新消息
     function tabSet() {
         $('.newstab').each(function() {
             var _tab = $(this),
@@ -593,6 +595,7 @@ $(function() {
         });
     }
 
+    // 常見問答
     function tabSet2() {
         $('.frequentlyQA_tab').each(function() {
             var _tab = $(this),
@@ -641,10 +644,65 @@ $(function() {
             }
         });
     }
+
+    // cptab
+    function tabSet() {
+        $('.cptab').each(function() {
+            var _tab = $(this),
+                _tabItem = _tab.find('.tabItem'),
+                // _tabItemA = _tabItem.children('a'), //改button後，這行沒有
+                _tabContent = _tab.find('.tabContent'),
+                tabwidth = _tab.width(),
+                tabItemHeight = _tabItem.outerHeight(),
+                tabContentHeight = _tab.find('.active').next().innerHeight(),
+                tiGap = 8,
+                tabItemLength = _tabItem.length,//有幾個頁籤
+                tabItemRows = parseInt(tabItemLength / 3) + 1, //頁籤有幾行
+                 tabItemHeight = _tabItem.outerHeight(true) * tabItemRows, //頁籤的總高度
+                tabItemWidth;
+            _tab.find('.active').next('.tabContent').show();
+            if (ww >= wwSmall) {
+                _tabContent.css('top', tabItemHeight);
+                _tab.height(tabContentHeight + tabItemHeight);
+               tabItemWidth = parseInt((tabwidth - 3 * tiGap) / 3); //每個頁籤的寬度
+                _tabItem.width(tabItemWidth).css('margin-left', tiGap);
+                // _tabItem.first().css('margin-left', 0);
+                // _tabItem.last().css({ 'position': 'absolute', 'top': 0, 'right': 0 }).width(tabItemWidth);
+            } else {
+                _tab.css('height', 'auto');
+                _tabItem.width(tabwidth);
+                _tabItem.css('margin-left', 0);
+            }
+            _tabItem.focus(tabs); //改button後，前面改_tabItem
+            _tabItem.click(tabs); //改button後，前面改_tabItem
+            function tabs(e) {
+                var _tabItemNow = $(this), //改button後，原來$(this).parent(),改$(this)
+                    tvp = _tab.offset().top,
+                    tabIndex = _tabItemNow.index() / 2,
+                    scollDistance = tvp + tabItemHeight * tabIndex - tab_headerHeight;
+                _tabItem.removeClass('active');
+                _tabItemNow.addClass('active');
+                if (ww <= wwSmall) {
+                    _tabItem.not('.active').next().slideUp();
+                    _tabItemNow.next().slideDown();
+                    $("html,body").stop(true, false).animate({ scrollTop: scollDistance });
+                } else {
+                    _tabItem.not('.active').next().hide();
+                    _tabItemNow.next().show();
+                    tabContentHeight = _tabItemNow.next().innerHeight();
+                    _tab.height(tabContentHeight + tabItemHeight);
+                }
+                e.preventDefault();
+            }
+        });
+    }
     $('.tabs>.tabItem:first-child>a').trigger('click');
     tabSet();
     $('.frequentlyQA_tab>.tabItem:first-child>a').trigger('click');
     tabSet2();
+
+    $('.cptab>.tabItem:first-child>a').trigger('click');
+    tabSet3();
     /*-----------------------------------*/
     ///////////////置頂go to top////////////
     /*-----------------------------------*/
